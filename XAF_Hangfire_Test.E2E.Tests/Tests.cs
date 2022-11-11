52efb9a7ee2da24f74d8ab10c9bdc8553d464d66
Felipe Ramos <felipe.ramos@tier2systems.com>
2022-11-11 11:24:30 +0000

    Add project files.

---
+++ XAF_Hangfire_Test.E2E.Tests/Tests.cs
@@ -0,0 +1,40 @@
+﻿using DevExpress.EasyTest.Framework;
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
+// To run functional tests for ASP.NET Web Forms and ASP.NET Core Blazor XAF Applications,
+// install browser drivers: https://www.selenium.dev/documentation/getting_started/installing_browser_drivers/.
+//
+// -For Google Chrome: download "chromedriver.exe" from https://chromedriver.chromium.org/downloads.
+// -For Microsoft Edge: download "msedgedriver.exe" from https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/.
+//
+// Selenium requires a path to the downloaded driver. Add a folder with the driver to the system's PATH variable.
+//
+// Refer to the following article for more information: https://docs.devexpress.com/eXpressAppFramework/403852/
+
+namespace XAF_Hangfire_Test.Module.E2E.Tests;
+
+public class XAF_Hangfire_TestTests : IDisposable {
+    const string BlazorAppName = "XAF_Hangfire_TestBlazor";
+    const string AppDBName = "XAF_Hangfire_Test";
+    EasyTestFixtureContext FixtureContext { get; } = new EasyTestFixtureContext();
+
+	public XAF_Hangfire_TestTests() {
+        FixtureContext.RegisterApplications(
+            new BlazorApplicationOptions(BlazorAppName, string.Format(@"{0}\..\..\..\..\XAF_Hangfire_Test.Blazor.Server", Environment.CurrentDirectory))
+        );
+        FixtureContext.RegisterDatabases(new DatabaseOptions(AppDBName, "XAF_Hangfire_TestEasyTest", server: @"(localdb)\mssqllocaldb"));	           
+	}
+    public void Dispose() {
+        FixtureContext.CloseRunningApplications();
+    }
+    [Theory]
+    [InlineData(BlazorAppName)]
+    public void Test(string applicationName) {
+        FixtureContext.DropDB(AppDBName);
+        var appContext = FixtureContext.CreateApplicationContext(applicationName);
+        appContext.RunApplication();
+        //appContext.Navigate("My Details");
+    }
+}
---
+++ XAF_Hangfire_Test.Module/BusinessObjects/Person.cs
@@ -0,0 +1,39 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XAF_Hangfire_Test.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    [ImageName("BO_Contact")]
+   
+    public class Person : BaseObject
+    { 
+        public Person(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+
+        public int Age { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string Name { get; set; }
+    }
+}
